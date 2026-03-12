bd901359defe18c2109702bbf05fe6540c9e81d7
clarehobson <clare_monahan@hotmail.com>
2026-03-12 03:00:08 +0000

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,47 @@
+var CACHE_NAME = 'cleanliving-v1';
+var urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(name) {
+          return name !== CACHE_NAME;
+        }).map(function(name) {
+          return caches.delete(name);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  // Network first, fall back to cache
+  event.respondWith(
+    fetch(event.request).then(function(response) {
+      // Clone and cache successful responses
+      if (response && response.status === 200) {
+        var responseClone = response.clone();
+        caches.open(CACHE_NAME).then(function(cache) {
+          cache.put(event.request, responseClone);
+        });
+      }
+      return response;
+    }).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
